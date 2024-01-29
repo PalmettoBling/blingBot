f93ab301b68588b54532a6024ede1e1a65ba701c
Charles Merrick Moss <merrickm@live.com>
2024-01-29 09:59:22 -0500

    Body Testing

--- commandHandler.cs
+++ commandHandler.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using FromBodyAttribute = Microsoft.Azure.Functions.Worker.Http.FromBodyAttribute;
 
 namespace Bling.Function
 {
@@ -15,10 +17,18 @@ namespace Bling.Function
         }
 
         [Function("commandHandler")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        //public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req, HttpRequestData reqData)
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req, [FromBody] BodyOfRequest reqData)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            _logger.LogInformation("Command Handler HTTP trigger function processed a request.");
+
+            _logger.LogInformation("Request Data Type: " + reqData.Type);
+            _logger.LogInformation("Request Data Name: " + reqData.Name);
+
+            return new OkObjectResult("Request Data: " + reqData.ToString());
+            //return new OkObjectResult("Welcome to Azure Functions!");
         }
     }
+
+    public record BodyOfRequest (int Type, string Name);
 }
